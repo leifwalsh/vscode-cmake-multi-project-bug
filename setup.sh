93f5dfd572f0cb08c9fac0676acfa4d764a20e44
Leif Walsh <leif.walsh@gmail.com>
2022-06-15 17:46:44 +0000

    switch to ninja

--- setup.sh
+++ setup.sh
@@ -6,19 +6,21 @@ set -u
 mkdir -p lib/build
 (cd lib/build;
     cmake \
+        -G Ninja \
         -DCMAKE_INSTALL_PREFIX=../dist \
         -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_CXX_COMPILER=clang++ \
         ../src &&
-    make install
+    ninja install
 )
 mkdir -p app/build
 (cd app/build;
     cmake \
+        -G Ninja \
         -DCMAKE_INSTALL_PREFIX=../dist \
         -DCMAKE_PREFIX_PATH=../../lib/dist \
         -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_CXX_COMPILER=clang++ \
         ../src &&
-    make install
+    ninja install
 )
